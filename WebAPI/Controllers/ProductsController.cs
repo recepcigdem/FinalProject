f0b935bedb0a0ede36141f9e893eac1c08484640
recepcigdem <recepcigdem24@gmail.com>
2021-03-13 11:05:39 +0300

    Cors eklendi.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Entities.Concrete;
@@ -23,6 +24,8 @@ namespace WebAPI.Controllers
         [HttpGet("getAll")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(2000);
+
             var result = _productService.GetAll();
             if (result.Success)
             {
